80336c09f09d8a51bb2e4639db2feccaa17b79f4
Nasir <nasir.ahmed@mobinius.com>
2015-09-29 20:12:21 +0530

    Created new separate EDLView class

---
+++ EDLite/EDLite/Managers/CBL/EDLViews.h
@@ -0,0 +1,22 @@
+//
+//  EDLViews.h
+//  EDLite
+//
+//  Created by Nasirahmed on 29/09/15.
+//  Copyright (c) 2015 Nasir. All rights reserved.
+//
+
+
+@interface EDLViews : NSObject
+
++(CBLView*)activeTicketListView:(CBLDatabase*)database;
+
++(CBLView*)completedTicketListView:(CBLDatabase*)database;
+
++(CBLView*)countOfMapsView:(CBLDatabase*)database;
+
++(CBLView*)countOfTicketsView:(CBLDatabase*)database;
+
++(CBLView*)typeProjectView:(CBLDatabase*)database;
+
+@end
---
+++ EDLite/EDLite/Managers/CBL/EDLViews.m
@@ -0,0 +1,81 @@
+//
+//  EDLViews.m
+//  EDLite
+//
+//  Created by Nasirahmed on 29/09/15.
+//  Copyright (c) 2015 Nasir. All rights reserved.
+//
+
+#import "EDLViews.h"
+
+@implementation EDLViews
+
++(CBLView*)activeTicketListView:(CBLDatabase*)database{
+    CBLView* activeTicketListView = [database viewNamed:@"_activeTicketListView"];
+    activeTicketListView.documentType = kTicketType;
+    if(!activeTicketListView.mapBlock){
+        [activeTicketListView setMapBlock:MAPBLOCK({
+            
+            BOOL isArchived = doc[@"archived"] != [NSNull null];
+            BOOL nonCompleted = ![[doc[@"state"][@"state"] lowercaseString] isEqualToString:@"completed"];
+            
+            if(isArchived && nonCompleted)
+                emit(doc[@"_id"],doc[@"_id"]);
+            
+        }) version:@"1.3"];
+    }
+    return activeTicketListView;
+}
+
++(CBLView*)completedTicketListView:(CBLDatabase*)database{
+    CBLView* completedTicketListView = [database viewNamed:@"_completedTicketListView"];
+    completedTicketListView.documentType = kTicketType;
+    if(!completedTicketListView.mapBlock){
+        [completedTicketListView setMapBlock:MAPBLOCK({
+            
+            BOOL isCompleted = [[doc[@"state"][@"state"] lowercaseString] isEqualToString:@"completed"];
+            
+            if(isCompleted)
+                emit(doc[@"_id"],doc[@"_id"]);
+            
+        }) version:@"1.2"];
+    }
+    return completedTicketListView;
+}
+
++(CBLView*)countOfMapsView:(CBLDatabase*)database{
+    CBLView* countOfMapsView = [database viewNamed:@"_countOfMapsView"];
+    countOfMapsView.documentType = kMapType;
+    if(!countOfMapsView.mapBlock){
+        [countOfMapsView setMapBlock:MAPBLOCK({
+            emit(doc[@"_id"],@1);
+        }) reduceBlock:REDUCEBLOCK({
+            return [CBLView totalValues:values];
+        }) version:@"1.0"];
+    }
+    return countOfMapsView;
+}
+
++(CBLView*)countOfTicketsView:(CBLDatabase*)database{
+    
+    CBLView* countOfTicketsView = [database viewNamed:@"_countOfTicketsView"];
+    countOfTicketsView.documentType = kTicketType;
+    if(!countOfTicketsView.mapBlock){
+        [countOfTicketsView setMapBlock:MAPBLOCK({
+            emit(doc[@"_id"],@1);
+        }) reduceBlock:REDUCEBLOCK({
+            return  [CBLView totalValues:values];
+        }) version:@"1.1"];
+    }
+    return countOfTicketsView;
+}
+
++(CBLView*)typeProjectView:(CBLDatabase*)database{
+    CBLView* typeProjectView = [database viewNamed:@"_typeProjectView"];
+    typeProjectView.documentType = kProjectDocumentType;
+    [typeProjectView setMapBlock:MAPBLOCK({
+        emit(doc[@"_id"],doc[@"_id"]);
+    }) version:@"1.0"];
+    return typeProjectView;
+}
+@end
